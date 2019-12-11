1fcdaeadb71d815d6509e373423720588f8fb725
Cole Finlayson <cole.fin8@gmail.com>
2019-12-11 12:15:32 -0700

    material

--- src/components/Topics/Topic.js
+++ src/components/Topics/Topic.js
@@ -1,1 +1,69 @@
 //Specific topic
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import "./Topics.css";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Container,
+  Card,
+  CardHeader,
+  CardContent,
+  Avatar,
+  CardActions
+} from "@material-ui/core";
+
+//this is all material ui stuff i couldn't get it to work in another file and then import it so it's here
+//----------------------------------------------------------------
+//see category.js for explanation
+//----------------------------------------------------------------
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    backgroundColor: "#607d8b"
+  },
+  headerRoot: {
+    width: "100%",
+    margin: 0,
+    padding: 0
+  },
+  avatar: {
+    position: "relative",
+    left: -10,
+    top: -10
+  },
+  title: {}
+}));
+//End of materialui stuff
+
+const Topic = props => {
+  const classes = useStyles();
+  console.log(props);
+  return (
+    //   Link goes to "Topics" page of corresponding category
+
+    <Container maxWidth="md">
+      <Link
+        to={`/category/${props.match.params.topicId}/${props.topic.id}`}
+        style={{ textDecoration: "none" }}
+      >
+        <Card
+          classes={{
+            root: classes.root
+          }}
+          className="category-box"
+          raised={true}
+        >
+          <CardHeader
+            classes={{ root: classes.headerRoot, avatar: classes.avatar }}
+            className="category-box-text"
+            title={props.topic.name}
+          />
+        </Card>
+      </Link>
+    </Container>
+  );
+};
+
+export default withRouter(Topic);
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,18 @@
-import React from 'react'
-import { Switch, Route} from 'react-router-dom'
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
 //! import components here
-import Categories from './components/Categories/Categories'
+import Categories from "./components/Categories/Categories";
+import Topics from "./components/Topics/Topics";
+import Deck from './components/Deck/Deck'
 
 export default (
-    <Switch>
-        <Route exact path ='/' component={Categories} />
-    </Switch>
-)
+  <Switch>
+    <Route exact path="/" component={Categories} />
+    <Route path="/category/:topicId" component={Topics} />
+    <Route path='/category/:topicId/:deckId' component={Deck}/>
+  </Switch>
+);
 
 // /category/:id - Topics
 
