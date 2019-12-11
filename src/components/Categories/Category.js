62f5954ccdceedd84c0d6dd88c9d9cde6151d38c
Cole Finlayson <cole.fin8@gmail.com>
2019-12-10 17:02:33 -0700

    pushing

--- src/components/Categories/Category.js
+++ src/components/Categories/Category.js
@@ -1,1 +1,20 @@
 //Specific Category Card
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Categories.css";
+import Container from "@material-ui/core/Container";
+
+const Category = props => {
+  return (
+    //   Link goes to "Topics" page of corresponding category
+    // <Link to={`/category/${props.id}`}>
+    <Container maxWidth="md" color="primary">
+      <div className="category-box">
+        <h1 className="category-box-text">{props.category.name}</h1>
+      </div>
+    </Container>
+    // </Link>
+  );
+};
+
+export default Category;
